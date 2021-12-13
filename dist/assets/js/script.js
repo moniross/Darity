f0136752a743b3f66f32e997aa482f4d02d074a9
moniross <monirfouzi20@gmail.com>
2021-12-13 11:35:56 +0100

    my transaction

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -129,3 +129,121 @@ $(document).ready(function () {
     });
 
 });
+
+// chart
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var data = {
+"labels": [
+"1 Aug",
+"2 Aug",
+"3 Aug",
+"4 Aug",
+"5 Aug",
+"6 Aug",
+"7 Aug",
+"8 Aug",
+"9 Aug",
+"10 Aug"
+],
+"datasets": [
+{
+  "label": "Category1",
+  "backgroundColor": "#000000",
+  "fill": false,
+  "data": [
+	"20",
+	"30",
+	"80",
+	"20",
+	"40",
+	"10",
+	"60"
+  ],
+  "borderColor": "#ff9300",
+  "pointRadius": "10",
+  "pointBackgroundColor": "#945200",
+  "pointBorderColor": "#ff9300",
+  "lineTension": 0.9,
+  "pointBorderWidth": 5,
+  "pointHoverBackgroundColor": "#ff9300",
+  "pointHoverBorderColor": "#945200"
+},
+{
+  "label": "Category2",
+  "backgroundColor": "",
+  "fill": false,
+  "data": [
+	"60",
+	"10",
+	"40",
+	"30",
+	"80",
+	"30",
+	"40"
+  ],
+  "borderColor": "#73fa79",
+  "pointRadius": "10",
+  "lineTension": 0,
+  "pointBackgroundColor": "#008f00",
+  "pointBorderColor": "#73fa79",
+  "pointBorderWidth": 5
+}
+]
+};
+var options = {
+"title": {
+"display": false
+},
+"legend": {
+"display": true
+},
+"scales": {
+"yAxes": [
+  {
+	"ticks": {
+	  "beginAtZero": true
+	},
+	"gridLines": {
+	  "display": true,
+	  "lineWidth": 1,
+	  "drawOnChartArea": true,
+	  "color": "#000000",
+	  "zeroLineColor": "#000000",
+	  "zeroLineWidth": 1,
+	  "drawTicks": true
+	}
+  }
+],
+"xAxes": {
+  "0": {
+	"gridLines": {
+	  "drawOnChartArea": false,
+	  "offsetGridLines": false,
+	  "zeroLineColor": "#000000",
+	  "display": true,
+	  "lineWidth": 2,
+	  "drawTicks": true,
+	  "zeroLineWidth": 2,
+	  "color": "#000000"
+	},
+	"ticks": {
+	  "display": true,
+	  "beginAtZero": true
+	}
+  }
+}
+},
+"elements": {
+"line": {
+  "borderColor": "#000000",
+  "lineTension": 0
+}
+}
+};
+
+var myChart = new Chart(ctx, {
+	type: 'line',
+	data: data,
+	options: options
+});

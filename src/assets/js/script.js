ee6d8c47009d9b5e03d15d2af19028635befd749
moniross <monirfouzi20@gmail.com>
2021-12-22 20:40:41 +0100

    add

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -243,3 +243,42 @@ var bar_chart = new Chart(gradientCtx, {
     ],
   },
 });
+
+// countdown
+
+function makeTimer() {
+  //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");
+  var endTime = new Date("29 April 2023 9:56:00 GMT+01:00");
+  endTime = Date.parse(endTime) / 1000;
+
+  var now = new Date();
+  now = Date.parse(now) / 1000;
+
+  var timeLeft = endTime - now;
+
+  var days = Math.floor(timeLeft / 86400);
+  var hours = Math.floor((timeLeft - days * 86400) / 3600);
+  var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
+  var seconds = Math.floor(
+    timeLeft - days * 86400 - hours * 3600 - minutes * 60
+  );
+
+  if (hours < "10") {
+    hours = "0" + hours;
+  }
+  if (minutes < "10") {
+    minutes = "0" + minutes;
+  }
+  if (seconds < "10") {
+    seconds = "0" + seconds;
+  }
+
+  $(".days").html(days + "<span></span>");
+  $(".hours").html(hours + "<span></span>");
+  $(".minutes").html(minutes + "<span></span>");
+  $(".seconds").html(seconds + "<span></span>");
+}
+
+setInterval(function () {
+  makeTimer();
+}, 1000);
